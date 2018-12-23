e8ed6a47fe35cda6b7af6ed6af1525f9a3033a3d
ANDeaD-K <andead@inbox.ru>
2018-12-23 11:15:18 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-TAG=$1
+#TAG=$1
 #DOCKER_USERNAME=$2
 #DOCKER_PASSWORD=$3
 
@@ -13,11 +13,11 @@ TAG=$1
 cp ./Dockerfile ./bin/Release/netcoreapp2.1/Dockerfile
 
 # Build the Docker images
-docker build -t andead/dotnet.jwt:$TAG bin/Release/netcoreapp2.1/.
-docker tag andead/dotnet.jwt:$TAG andead/dotnet.jwt:latest
+docker build -t andead/dotnet.jwt:latest bin/Release/netcoreapp2.1/.
+#docker tag andead/dotnet.jwt:$TAG andead/dotnet.jwt:latest
 
 # Login to Docker Hub and upload images
 #docker login --username=$2 --password=$3
-docker login --username $2 --password $3
-docker push repository/project:$TAG
-docker push repository/project:latest
+docker login -u $DOCKER_LOGIN_USERNAME -p $DOCKER_LOGIN_PASSWORD
+#docker push andead/dotnet.jwt:$TAG
+docker push andead/dotnet.jwt:latest
